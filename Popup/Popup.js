a849e8a6845d4846228f5d1a32796363ad3963d8
TNlap <phnt0604@gmail.com>
2024-07-23 07:23:26 +0700

    update

---
+++ Popup/Popup.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function OpenPopup (){
+    popup.classList.add("open-popup");
+}
+
+function CloseupPoppup (){
+   popup.classList.remove("open-popup");
+}
